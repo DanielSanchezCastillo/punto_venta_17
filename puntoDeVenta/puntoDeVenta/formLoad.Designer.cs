ec8e58cd6f54259b3c52c58d520623861e266b30
DanielSanchezCastillo <47910493+DanielSanchezCastillo@users.noreply.github.com>
2021-03-23 11:20:15 -0600

    rama de actualización
    se agregaron dos formularios y mejoro funcionalidad de botones de inicio de sesión.
    add:
    * form load
    *form dash
    *class security and encrypt
    designer:
    * colors
    * buttons
    Functions:
    * close app and others

---
+++ puntoDeVenta/puntoDeVenta/formLoad.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace puntoDeVenta
+{
+    partial class formCharge
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblPunto = new System.Windows.Forms.Label();
+            this.lblDes = new System.Windows.Forms.Label();
+            this.picIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(184)))), ((int)(((byte)(206)))));
+            this.progressBar1.Location = new System.Drawing.Point(0, 465);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(319, 10);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblPunto
+            // 
+            this.lblPunto.AutoSize = true;
+            this.lblPunto.Font = new System.Drawing.Font("Arial Black", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblPunto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(184)))), ((int)(((byte)(206)))));
+            this.lblPunto.Location = new System.Drawing.Point(12, 209);
+            this.lblPunto.Name = "lblPunto";
+            this.lblPunto.Size = new System.Drawing.Size(216, 33);
+            this.lblPunto.TabIndex = 1;
+            this.lblPunto.Text = "Punto de Venta";
+            // 
+            // lblDes
+            // 
+            this.lblDes.AutoSize = true;
+            this.lblDes.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblDes.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(78)))), ((int)(((byte)(184)))), ((int)(((byte)(206)))));
+            this.lblDes.Location = new System.Drawing.Point(146, 286);
+            this.lblDes.Name = "lblDes";
+            this.lblDes.Size = new System.Drawing.Size(123, 20);
+            this.lblDes.TabIndex = 2;
+            this.lblDes.Text = "by Daniel Sánchez";
+            // 
+            // picIcon
+            // 
+            this.picIcon.Location = new System.Drawing.Point(90, 38);
+            this.picIcon.Name = "picIcon";
+            this.picIcon.Size = new System.Drawing.Size(114, 96);
+            this.picIcon.TabIndex = 3;
+            this.picIcon.TabStop = false;
+            // 
+            // formCharge
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(36)))), ((int)(((byte)(49)))));
+            this.ClientSize = new System.Drawing.Size(319, 477);
+            this.ControlBox = false;
+            this.Controls.Add(this.picIcon);
+            this.Controls.Add(this.lblDes);
+            this.Controls.Add(this.lblPunto);
+            this.Controls.Add(this.progressBar1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "formCharge";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "formCharge";
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblPunto;
+        private System.Windows.Forms.Label lblDes;
+        private System.Windows.Forms.PictureBox picIcon;
+    }
+}
